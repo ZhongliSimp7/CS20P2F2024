6a06158faa15a76033909e722ab2a87e7e45cac0
1100102540 <1100102540@L815WD220028.edu.cbe.ab.ca>
2024-09-18 11:38:30 -0600

    Moo

---
+++ Chapter3/src/Mastery/Project.java
@@ -0,0 +1,53 @@
+package Mastery;
+
+//Need a header
+
+//Import section
+import java.util.*;
+import java.text.*;
+
+public class Project {
+
+	public static void main(String[] args) {
+		
+		//Declaration 
+		
+		int designing, coding, debugging, testing;
+		double totalMinutes, totalDesign, totalCode, totalDebug, totalTest;
+		
+      //Prompt the user to get all the data for some of the above variables
+		
+		Scanner Number = new Scanner(System.in);
+		System.out.println("Enter the minutes you spent on the following tasks- ");
+		System.out.print("Designing the code- ");
+		designing = Number.nextInt();
+		System.out.print("Coding- ");
+		coding = Number.nextInt();
+		System.out.print("Debugging the code- ");
+		debugging = Number.nextInt();
+		System.out.print("Testing- ");
+		testing = Number.nextInt();
+
+      //Calculate the information for some of the above variables
+		totalMinutes = designing + coding + debugging + testing;
+		totalDesign = 100*designing/totalMinutes;
+		totalCode = 100*coding/totalMinutes;
+		totalDebug = 100*debugging/totalMinutes;
+		totalTest = 100*testing/totalMinutes;
+		
+	  //Using the DecimalFormat class to limit decimal places in final output to 2
+		DecimalFormat numberFormat = new DecimalFormat("#.00");
+
+      //Output to the console all the required variables for this assignment
+		System.out.println("");
+		System.out.println("Task                 % Time");
+		System.out.println("");
+		System.out.println("Designing            " + numberFormat.format(totalDesign) + " %");
+		System.out.println("Coding               " + numberFormat.format(totalCode) + " %");
+		System.out.println("Debugging            " + numberFormat.format(totalDebug) + " %");
+		System.out.println("Testing              " + numberFormat.format(totalTest) + " %");
+
+  }
+}
+
+//Screen Dump required
