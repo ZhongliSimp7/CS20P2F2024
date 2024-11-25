02a251f35a3c32dd79b668338b987139e7327e22
1100102540 <1100102540@L815WD220028.edu.cbe.ab.ca>
2024-11-25 11:57:43 -0700

    E

--- Chapter7/src/Mastery/LunchOrder.java
+++ Chapter7/src/Mastery/LunchOrder.java
@@ -2,54 +2,77 @@ package Mastery;
 
 public class LunchOrder {
 	
-	private int numHam;
-	private int numSal;
-	private int numFre;
-	private int numSod;
+	private double numHam;
+	private double numSal;
+	private double numFre;
+	private double numSod;
+
+	private double Fat;
+	private double Carbs;
+	private double Fiber;
 	
 	private double ham = 1.85;
 	private double salad = 2.00;
 	private double fries = 1.30;
 	private double soda = 0.95;
+	
+	// All the methods for ham
+	 public void Ham(double i, double f, double c, double fi) {
+	 numHam = i; 
+	 Fat = f;
+	 Carbs = c;
+	 Fiber = fi; }
+	 
+	 public double HamFat() {
+		 return Fat; }
+	 public double HamCarb() {
+		 return Carbs; }
+	 public double HamFibre() {
+		 return Fiber; }
 
-	 /*
-	 * Sets number of the Ham as the input.
-	 * pre: none
-	 * post: Number of ham has been set.
-	 */
-	 public void Ham(int i) {
-	 numHam = i; }
+	 // All the methods for salad
+	 public void Salad(double i, double f, double c, double fi) {
+	 numSal = i; 
+	 Fat = f;
+	 Carbs = c;
+	 Fiber = fi; }
 	 
+	 public double SalFat() {
+		 return Fat; }
+	 public double SalCarb() {
+		 return Carbs; }
+	 public double SalFibre() {
+		 return Fiber; }
 	 
-	 /*
-	 * Sets number of the Salad as the input.
-	 * pre: none
-	 * post: Number of salad has been set.
-	 */
-	 public void Salad(int i) {
-	 numSal = i; }
-		 
-	 /*
-	 * Sets number of the fries as the input.
-	 * pre: none
-	 * post: Number of fries has been set.
-	 */
-	 public void Fries(int i) {
-	 numFre = i; }
-			 
-	 /*
-	 * Sets number of the soda as the input.
-	 * pre: none
-	 * post: Number of soda has been set.
-	 */
-	 public void Soda(int i) {
-	 numSod = i; }
+	 // All the methods for fries
+	 public void Fries(double i, double f, double c, double fi) {
+	 numFre = i; 
+	 Fat = f;
+	 Carbs = c;
+	 Fiber = fi; }
 	 
-	 /*
-	 * Calculates and returns total cost of order.
-	 * pre: none
-	 * post: Total has been returned.
-	 */
+	 public double FriFat() {
+		 return Fat; }
+	 public double FriCarb() {
+		 return Carbs; }
+	 public double FriFibre() {
+		 return Fiber; }
+	 
+	 // All the methods for soda
+	 public void Soda(double i, double f, double c, double fi) {
+	 numSod = i; 
+	 Fat = f;
+	 Carbs = c;
+	 Fiber = fi; }
+	 
+	 public double SodFat() {
+		 return Fat; }
+	 public double SodCarb() {
+		 return Carbs; }
+	 public double SodFibre() {
+		 return Fiber; }
+	 
+	 // Method for calculating and displaying the total
 	 public double Total() {
 		 double total = numHam*ham + numSal*salad + numFre*fries + numSod*soda;
 		 return(total);
--- Chapter7/src/Mastery/LunchOrderClient.java
+++ Chapter7/src/Mastery/LunchOrderClient.java
@@ -19,27 +19,44 @@ public class LunchOrderClient {
 
 	public static void main(String[] args) {
 		
+		// Creating the object
 		LunchOrder Food = new LunchOrder(); 
 		Scanner Input = new Scanner(System.in);
 		System.out.println("Welcome to Lunch Order");
 		
+		// Asks user for a prompt for how many burgers they would like
+		// Then it displays the nutritional info after feeding all the values into Food.Ham()
 		System.out.print("Enter the number of hamburgers - ");
-		int numHam = Input.nextInt();
-		Food.Ham(numHam);
+		double numHam = Input.nextDouble();
+		Food.Ham(numHam, 9.0, 33.0, 1.0);
+		System.out.print("Each hamburger has " + Food.HamFat() + "g of fat, ");  
+		System.out.print(Food.HamCarb() + "g of carbs, and " + Food.HamFibre() + " of fiber \n");
+		System.out.println("");
 		
+		// Similar thing here, continuing for each type of food item
 		System.out.print("Enter the number of salads - ");
-		int numSal = Input.nextInt();
-		Food.Salad(numSal);
+		double numSal = Input.nextDouble();
+		Food.Salad(numSal, 1.0, 11.0, 5.0);
+		System.out.print("Each salad has " + Food.SalFat() + "g of fat, ");  
+		System.out.print(Food.SalCarb() + "g of carbs, and " + Food.SalFibre() + " of fiber \n");
+		System.out.println("");
 		
 		System.out.print("Enter the number of french fries - ");
-		int numFre = Input.nextInt();
-		Food.Fries(numFre);
+		double numFre = Input.nextDouble();
+		Food.Fries(numFre, 11.0, 36.0, 5.0);
+		System.out.print("Each pack of french fries has " + Food.FriFat() + "g of fat, ");  
+		System.out.print(Food.FriCarb() + "g of carbs, and " + Food.FriFibre() + " of fiber \n");
+		System.out.println("");
 		
 		System.out.print("Enter the number of sodas - ");
-		int numSod = Input.nextInt();
-		Food.Soda(numSod);
+		double numSod = Input.nextDouble();
+		Food.Soda(numSod, 0.0, 38.0, 0.0);
+		System.out.print("Each soda has " + Food.SodFat() + "g of fat, ");  
+		System.out.print(Food.SodCarb() + "g of carbs, and " + Food.SodFibre() + " of fiber \n");
+		System.out.println("");
 		
-		System.out.println("Your order comes to " + Food.Total());
+		// Lastly it just prints out the total for the user's order
+		System.out.println("Your order comes to $" + Food.Total());
 		
 		
 		/*
@@ -55,6 +72,39 @@ public class LunchOrderClient {
 
 Screen Dump (2 Test Cases) - 
 
+-
+
+Welcome to Lunch Order
+Enter the number of hamburgers - 3
+Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0 of fiber 
+
+Enter the number of salads - 2
+Each salad has 1.0g of fat, 11.0g of carbs, and 5.0 of fiber 
+
+Enter the number of french fries - 4
+Each pack of french fries has 11.0g of fat, 36.0g of carbs, and 5.0 of fiber 
+
+Enter the number of sodas - 1
+Each soda has 0.0g of fat, 38.0g of carbs, and 0.0 of fiber 
+
+Your order comes to 15.7
+
+-
+
+Welcome to Lunch Order
+Enter the number of hamburgers - 0
+Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0 of fiber 
+
+Enter the number of salads - 1
+Each salad has 1.0g of fat, 11.0g of carbs, and 5.0 of fiber 
+
+Enter the number of french fries - 2
+Each pack of french fries has 11.0g of fat, 36.0g of carbs, and 5.0 of fiber 
+
+Enter the number of sodas - 1
+Each soda has 0.0g of fat, 38.0g of carbs, and 0.0 of fiber 
+
+Your order comes to $5.55
 
 
 */
