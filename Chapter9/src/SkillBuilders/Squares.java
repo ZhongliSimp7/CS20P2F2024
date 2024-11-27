644db0c07ae546a33b816bc53cb4d0eb8d90809c
1100102540 <1100102540@L815WD220029.edu.cbe.ab.ca>
2024-11-27 11:58:30 -0700

    E

---
+++ Chapter9/src/SkillBuilders/Squares.java
@@ -0,0 +1,10 @@
+package SkillBuilders;
+
+public class Squares {
+
+	public static void main(String[] args) {
+			
+			
+	
+	}
+}
---
+++ Chapter9/src/SkillBuilders/StudentRoster.java
@@ -0,0 +1,18 @@
+package SkillBuilders;
+import java.util.*;
+
+public class StudentRoster {
+
+	public static void main(String[] args) {
+		
+		Scanner Input = new Scanner(System.in);
+		String[] students;
+		System.out.print("Enter the number of students in the class - ");
+		int number = Input.nextInt();
+		students = new String[number];
+		
+		System.out.println("System Roster");
+		
+
+	}
+}
